f545942bf4c1a0b559330cec8f85569ca4f7af18
webdmamune <webd.mamun@gmail.com>
2022-08-08 02:47:31 +0600

    [more-section]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import About from "./components/About";
+import AllInOne from "./components/AllInOne";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Support from "./components/Support";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
+      <Support />
+      <AllInOne />
     </>
   );
 }
